e0fca6014482766c821721d116feb6afe8a5fe6b
Teo <teo_chiaburu@yahoo.com>
2022-07-14 21:49:57 +0200

    A few tweaks for creating metafiles

--- scripts/create_metafiles_all.py
+++ scripts/create_metafiles_all.py
@@ -8,19 +8,21 @@ sys.path.insert(1, '../beexplainable')
 import glob
 from beexplainable.utils import metafile_readers as mr
 
+bees_folder = 'Bees_Christian/' #'Bees' for training set, 'Bees_Christian' for test set
+
 # Should the 4 similar Bombus species be compressed into one single species?
 compress_B_lucorum = True
 bees_subfolder = '22_species/' if compress_B_lucorum else '25_species/'
 
 # Read class names
-CLASSES_PATH = "../metafiles/Bees/" + bees_subfolder + "classes.txt"
+CLASSES_PATH = "../metafiles/" + bees_folder + bees_subfolder + "classes.txt"
 cls_dict = mr.metafile_to_dict(CLASSES_PATH)
 
-images = open("../metafiles/Bees/" + bees_subfolder + "images.txt", "a")
-image_class_labels = open("../metafiles/Bees/" + bees_subfolder + "image_class_labels.txt", "a")
+#images = open("../metafiles/" + bees_folder + "images.txt", "a")
+image_class_labels = open("../metafiles/" + bees_folder + bees_subfolder + "image_class_labels.txt", "a")
 
-BEES_PATH = '../../../data/data_lstudio/Bees/'
-img_paths = glob.glob(BEES_PATH + '**/*.jpg') # double ** for recursive search (over 30k imgs)
+BEES_PATH = '../../../data/data_lstudio/' + bees_folder
+img_paths = glob.glob(BEES_PATH + '*.jpg') if bees_folder == 'Bees_Christian/' else glob.glob(BEES_PATH + '**/*.jpg') # double ** for recursive search (over 30k imgs)
 
 for i in range(len(img_paths)):
 
@@ -32,13 +34,13 @@ for i in range(len(img_paths)):
     substrs = file_name.split('_')
     cls = substrs[0] + '_' + substrs[1]
 
+    # Images are stored in extra species subfolders
+    #images.write(str(i + 1) + ' ' + cls + '/' + file_name + '\n')
+
     # Compress Bombus if needed
     if compress_B_lucorum:
-        images.write(str(i + 1) + ' ' + cls + '/' + file_name + '\n') # images are stored in extra species subfolders
         if cls in ['Bombus_cryptarum', 'Bombus_magnus', 'Bombus_terrestris']:
             cls = 'Bombus_lucorum'
-    else:
-        images.write(str(i + 1) + ' ' + file_name + '\n')
 
     # Get class index from its name and write the pair (file_id, cls_id) to file
     cls_ind = list(cls_dict.keys())[list(cls_dict.values()).index(cls)]
@@ -46,5 +48,5 @@ for i in range(len(img_paths)):
 
     if i % 1000 == 0: print(i)
 
-images.close()
+#images.close()
 image_class_labels.close()
