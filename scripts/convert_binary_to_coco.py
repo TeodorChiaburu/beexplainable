82d9fa5a50545e51f585265b98d2b34818a3ec81
Teo <teo_chiaburu@yahoo.com>
2022-07-16 17:34:23 +0200

    Split mini set intro train and val

--- scripts/convert_binary_to_coco.py
+++ scripts/convert_binary_to_coco.py
@@ -10,24 +10,22 @@ from beexplainable.utils import annot_computers as ac
 # Metafile paths
 BEES_PATH = '../../../data/data_lstudio/Bees_Christian/'
 IMAGES_PATH = '../metafiles/Bees_Christian/images.txt'
-IMAGES_LABELS_PATH = '../metafiles/Bees_Christian/22_species/image_class_labels.txt'
-CLASSES_PATH = '../metafiles/Bees_Christian/22_species/classes.txt'
 
-# Read metafiles into dictionaries
+# Read metafiles into dictionary
 img_dict = mr.metafile_to_dict(IMAGES_PATH)
-img_lab_dict = mr.metafile_to_dict(IMAGES_LABELS_PATH)
-cls_dict = mr.metafile_to_dict(CLASSES_PATH)
 
 # Define json file for COCO data; it will include lists for categories/classes, images and annotations
-coco_json, images, annotations = {}, [], []
-categories = [{"id": int(c), "name": cls_dict[c]} for c in cls_dict]
-coco_json["categories"] = categories
+coco_json_train, coco_json_val = {}, {}
+images, annotations = [], []
+categories = [{"id": 0, "name": 'Wild_Bee'}]
+coco_json_train["categories"] = coco_json_val["categories"] = categories
 
 # Iterate through images and create json entries
 threshold = 50  # see ac.rle_to_matrix()
+num_train = 500 # how many images should go into the json used for training
 for i in range(1, len(img_dict)):
 
-    file_name, cls_ind = img_dict[str(i)], img_lab_dict[str(i)]
+    file_name = img_dict[str(i)]
     data = np.array(Image.open(BEES_PATH + file_name))[:, :, 0] # 1st channel is enough
 
     # Create boolean mask from array (through jpg-interpolation other values other than 0 and 255 are generated)
@@ -46,14 +44,17 @@ for i in range(1, len(img_dict)):
     # Create json for COCO
     images.append( {'id': i, 'width': w, 'height': h, 'file_name': file_name} )
     annotations.append( {'segmentation': rle, 'bbox': bbox, 'area': area,
-                         'image_id': i, 'category_id': int(cls_ind), 'iscrowd': 0, 'id': i} )
+                         'image_id': i, 'category_id': 0, 'iscrowd': 0, 'id': i} )
 
     if i%10 == 0: print(i)
 
-# Add lists of images and annotations to the json
-coco_json["images"] = images
-coco_json["annotations"] = annotations
+# Add lists of images and annotations to the validation json
+coco_json_train["images"], coco_json_val["images"] = images[:num_train], images[num_train:]
+coco_json_train["annotations"], coco_json_val["annotations"] = annotations[:num_train], annotations[num_train:]
 
-annotations_file = '../metafiles/Bees_Christian/22_species/bees_coco.json'
+annotations_file = '../metafiles/Bees_Christian/22_species/bees_coco_train.json'
 with open(annotations_file, 'w', encoding='utf-8') as f:
-    json.dump(coco_json, f, ensure_ascii=True, indent=4)
+    json.dump(coco_json_train, f, ensure_ascii=True, indent=4)
+annotations_file = '../metafiles/Bees_Christian/22_species/bees_coco_val.json'
+with open(annotations_file, 'w', encoding='utf-8') as f:
+    json.dump(coco_json_val, f, ensure_ascii=True, indent=4)
---
+++ scripts/split_mini_train_val.py
@@ -0,0 +1,21 @@
+"""Script to split annotated images into training and validation sets (needed to train YOLO)"""
+
+import shutil
+from beexplainable.utils import metafile_readers as mr
+
+# Data and metafile paths
+BEES_PATH  = '../../../data/data_lstudio/Bees_Christian/'
+TRAIN_PATH = '../../../data/data_lstudio/Bees_Christian_train/'
+VAL_PATH   = '../../../data/data_lstudio/Bees_Christian_val/'
+IMAGES_PATH = '../metafiles/Bees_Christian/images.txt'
+
+# Read metafiles into dictionary
+img_dict = mr.metafile_to_dict(IMAGES_PATH)
+
+num_train = 500 # same as in convert_binary_to_coco.py
+for i in range(1, len(img_dict)):
+    file_name = img_dict[str(i)]
+    if i <= num_train:
+        shutil.copy(BEES_PATH + file_name, TRAIN_PATH + file_name)
+    else:
+        shutil.copy(BEES_PATH + file_name, VAL_PATH + file_name)
