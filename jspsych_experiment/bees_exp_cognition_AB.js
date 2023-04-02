71867f670e950e8a33a1762632fbe0599bc9d1c9
Teo <teo_chiaburu@yahoo.com>
2023-04-02 15:35:19 +0200

    Redesigned experiment for random sampling from wider image pools.

--- jspsych_experiment/bees_exp_cognition_AB.js
+++ jspsych_experiment/bees_exp_cognition_AB.js
@@ -10,62 +10,157 @@ function choose_group(groups) {
   return groups[index];
 }
 assigned_group = choose_group(groups);
-console.log(assigned_group);
 
 // Create timeline
 var timeline = [];
 const protos = ["img/Andrena_flavipes_proto.jpg", "img/Andrena_fulva_proto.jpg", "img/Bombus_lucorum_proto.jpg"];
 
 // The 3 possible classes
-// Note: also used as label vector for Task 2
 const classes = ["Andrena flavipes", "Andrena fulva", "Bombus lucorum"];
 
-const test_samples   = ["img/Andrena_flavipes_56925373_1.jpg",
-                        "img/Andrena_fulva_24881697_4.jpg",
-                        "img/Bombus_terrestris_48053523_1.jpg",
-                        "img/Andrena_flavipes_68809988_2.jpg",
-                        "img/Andrena_fulva_12231414_3.jpg",
-                        "img/Bombus_terrestris_48213526_1.jpg"
-                       ];
+/*** Task 1 and 3 ***/
+// Sample pools for Tasks 1 and 3 (only 4 will be randomly picked for each species)
+const pool_samples_flavi = ["img/Andrena_flavipes_28862388_1.jpg",
+                            "img/Andrena_flavipes_33657115_1.jpg",
+                            "img/Andrena_flavipes_35698472_2.jpg",
+                            "img/Andrena_flavipes_40178244_1.jpg",
+                            "img/Andrena_flavipes_40832925_2.jpg",
+                            "img/Andrena_flavipes_40832925_3.jpg",
+                            "img/Andrena_flavipes_41163118_3.jpg",
+                            "img/Andrena_flavipes_41517541_1.jpg",
+                            "img/Andrena_flavipes_41517541_3.jpg",
+                            "img/Andrena_flavipes_42347630_1.jpg",
+                            "img/Andrena_flavipes_5795371_2.jpg",
+                            "img/Andrena_flavipes_8430646_1.jpg"];
+const pool_samples_fulva = ["img/Andrena_fulva_11872393_2.jpg",
+                            "img/Andrena_fulva_15143858_1.jpg",
+                            "img/Andrena_fulva_21366312_1.jpg",
+                            "img/Andrena_fulva_21567123_1.jpg",
+                            "img/Andrena_fulva_24609371_2.jpg",
+                            "img/Andrena_fulva_24609371_6.jpg",
+                            "img/Andrena_fulva_24752696_1.jpg",
+                            "img/Andrena_fulva_25074448_1.jpg",
+                            "img/Andrena_fulva_43114108_1.jpg",
+                            "img/Andrena_fulva_43172780_1.jpg",
+                            "img/Andrena_fulva_43270327_1.jpg",
+                            "img/Andrena_fulva_43280860_1.jpg"];
+const pool_samples_lucor = ["img/Bombus_terrestris_48029355_1.jpg",
+                            "img/Bombus_terrestris_48056955_1.jpg",
+                            "img/Bombus_terrestris_50405525_1.jpg",
+                            "img/Bombus_terrestris_53709769_1.jpg",
+                            "img/Bombus_terrestris_53737090_1.jpg",
+                            "img/Bombus_terrestris_53835710_1.jpg",
+                            "img/Bombus_terrestris_54871905_2.jpg",
+                            "img/Bombus_terrestris_55046379_1.jpg",
+                            "img/Bombus_terrestris_55141300_1.jpg",
+                            "img/Bombus_terrestris_55204752_1.jpg",
+                            "img/Bombus_terrestris_56168748_1.jpg",
+                            "img/Bombus_terrestris_70317519_1.jpg"];
 
-// Shuffle indexes for test samples in Tasks 1 and 3
-inds = Object.keys(test_samples);
-for (let i = inds.length - 1; i > 0; i--) {
+const pool_samples_13 = [pool_samples_flavi, pool_samples_fulva, pool_samples_lucor];
+
+// Randomly pick samples out of pool for T1&3
+const test_samples = [];
+const labels_13 = Array(4).fill(classes[0]).concat(Array(4).fill(classes[1]), Array(4).fill(classes[2])); // needed for checking user's correctness
+
+for (let i = 0; i < pool_samples_13.length; i++) {
+
+  // Shuffle pool for current species
+  for (let j = pool_samples_13[i].length - 1; j > 0; j--) {
+    const k = Math.floor(Math.random() * (j + 1));
+    [pool_samples_13[i][j], pool_samples_13[i][k]] = [pool_samples_13[i][k], pool_samples_13[i][j]];
+  }
+
+  // Add first new 6 samples to the list for T1&3
+  for (let j = 0; j < 4; j++) {
+    test_samples.push(pool_samples_13[i][j]);
+  }
+}
+
+// Shuffle indexes for test samples in Tasks 1&3 (and their corresponding true labels)
+// Otherwise, species order is not random
+for (let i = test_samples.length - 1; i > 0; i--) {
   const j = Math.floor(Math.random() * (i + 1));
-  [inds[i], inds[j]] = [inds[j], inds[i]];
-
+  [test_samples[i], test_samples[j]] = [test_samples[j], test_samples[i]];
+  [labels_13[i], labels_13[j]] = [labels_13[j], labels_13[i]];
 }
 
-// Shuffle test smaples and labels for Tasks 1 and 3
-const test_samples_1 = [];
-const labels_1 = [];
-const test_samples_2 = [];
-const labels_2 = [];
-for (let i = 0; i < 6; i++) {
-  test_samples_1.push(test_samples[parseInt(inds[i])]);
-  labels_1.push(classes[parseInt(inds[i]) % 6]); // test_samples are sorted according to classes vector i.e. the 2nd and the 8th samples will be A. flavipes
+const test_samples_1 = test_samples.slice(0, 6);
+const labels_1 = labels_13.slice(0, 6);
+const test_samples_3 = test_samples.slice(6); // start at index 6 till the end
+const labels_3 = labels_13.slice(6);
+
+/*** Task 2 ***/
+// Textual formulations of the given concepts
+const natural_exps = ['it has a smooth shiny dark brown texture', 'it is fuzzy dark orange', 'it has fuzzy yellow and black stripes']
+const natural_exps_neg = ['it does NOT have a smooth shiny dark brown texture',
+                          'it is NOT fuzzy dark orange', 'it does NOT have fuzzy yellow and black stripes']
+
+// Pool of correctly predicted samples (both cls and concept) - 5 will be picked
+const pool_samples_correct = ["img/Andrena_flavipes_10588940_2.jpg",
+                              "img/Andrena_flavipes_10588940_5.jpg",
+                              "img/Andrena_flavipes_22101904_1.jpg",
+                              "img/Andrena_flavipes_22465388_1.jpg",
+                              "img/Andrena_flavipes_33484614_1.jpg",
+                              "img/Andrena_fulva_12231414_2.jpg",
+                              "img/Andrena_fulva_16795311_1.jpg",
+                              "img/Andrena_fulva_24881697_3.jpg",
+                              "img/Andrena_fulva_41623103_3.jpg",
+                              "img/Andrena_fulva_41637839_1.jpg",
+                              "img/Andrena_fulva_41705690_1.jpg",
+                              "img/Bombus_terrestris_48213526_1.jpg",
+                              "img/Bombus_terrestris_50304865_1.jpg",
+                              "img/Bombus_terrestris_54307855_2.jpg",
+                              "img/Bombus_terrestris_54718964_2.jpg"];
+// Correct cls predictions
+const labels_samples_correct = Array(5).fill(classes[0]).concat(Array(6).fill(classes[1]), Array(4).fill(classes[2]));
+// Correct concept predictions
+const exps_correct_flavi = natural_exps[0] + ' but ' + natural_exps_neg[1] + ' and ' + natural_exps_neg[2];
+const exps_correct_fulva = natural_exps[1] + ' but ' + natural_exps_neg[0] + ' and ' + natural_exps_neg[2];
+const exps_correct_lucor = natural_exps[2] + ' but ' + natural_exps_neg[0] + ' and ' + natural_exps_neg[1];
+const exps_samples_correct = Array(5).fill(exps_correct_flavi).concat(Array(6).fill(exps_correct_fulva), Array(4).fill(exps_correct_lucor));
+
+// Shuffle samples, true labels and true explanations (take first 5)
+for (let i = pool_samples_correct.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1));
+  [pool_samples_correct[i], pool_samples_correct[j]] = [pool_samples_correct[j], pool_samples_correct[i]];
+  [labels_samples_correct[i], labels_samples_correct[j]] = [labels_samples_correct[j], labels_samples_correct[i]];
+  [exps_samples_correct[i], exps_samples_correct[j]] = [exps_samples_correct[j], exps_samples_correct[i]];
 }
-for (let i = 6; i < 12; i++) {
-  test_samples_2.push(test_samples[parseInt(inds[i])]);
-  labels_2.push(classes[parseInt(inds[i]) % 6]);
+
+const samples_correct = pool_samples_correct.slice(0, 5);
+const labels_correct = labels_samples_correct.slice(0, 5); // would be same as preds_correct -> no need for extra array
+const exps_correct = exps_samples_correct.slice(0, 5);
+
+// Pool of wrongly predicted samples (neither cls, nor concept) - 1 will be picked
+const pool_samples_wrong = ["img/Andrena_flavipes_68809988_1.jpg",
+                            "img/Bombus_terrestris_67926127_1.jpg",
+                            "img/Bombus_terrestris_93877801_3.jpg"];
+// Wrong cls prediction and wrong explanations
+// Note: every wrong sample was predicted A. fulva because fuzzy orange
+const labels_wrong = ["Andrena flavipes", "Bombus lucorum", "Bombus lucorum"];
+const pred_wrong = "Andrena fulva";
+const exp_wrong = exps_correct_fulva;
+
+// Pick one random wrongly predicted sample
+wrong_ind = choose_group([0, 1, 2]);
+const sample_wrong = pool_samples_wrong[wrong_ind];
+const label_wrong = labels_wrong[wrong_ind];
+
+// Concatenate and shuffle samples for Task 2, along with true labels, cls predictions and explanations
+const test_samples_2 = samples_correct.concat(sample_wrong);
+const labels_2 = labels_correct.concat(label_wrong);
+const preds_2 = labels_correct.concat(pred_wrong);
+const exps_2 = exps_correct.concat(exp_wrong);
+for (let i = 0; i < test_samples_2.length; i++) {
+  const j = Math.floor(Math.random() * (i + 1));
+  [test_samples_2[i], test_samples_2[j]] = [test_samples_2[j], test_samples_2[i]];
+  [labels_2[i], labels_2[j]] = [labels_2[j], labels_2[i]];
+  [preds_2[i], preds_2[j]] = [preds_2[j], preds_2[i]];
+  [exps_2[i], exps_2[j]] = [exps_2[j], exps_2[i]];
 }
 
-const pred_samples = ["img/Andrena_flavipes_5795371_2.jpg",   // ok-ok
-                      "img/Andrena_fulva_11872393_2.jpg",     // ok-no
-                      "img/Bombus_terrestris_68122824_2.jpg"  // no-ok
-                     ];
-// Model's class predictions for pred_samples
-const preds = ["Andrena flavipes", "Andrena fulva", "Bombus hortorum"];
-
-// Model's concept predictions for pred_samples
-const natural_exps = ['it is fuzzy dark orange', 'it has fuzzy yellow and black stripes', 'it has a smooth shiny dark brown texture']
-const exps = [natural_exps[0] + ' and ' + natural_exps[2],
-              natural_exps[2],
-              natural_exps[0] + ' and ' + natural_exps[2],
-              natural_exps[1],
-              natural_exps[1],
-              natural_exps[1]];
-
+/*** UI ***/
 // Grid with protos (just labels beneath, no buttons)
 var proto_grid = `
       <div id="div_row">
@@ -99,8 +194,8 @@ var preload = {
     type: jsPsychPreload,
     images: [protos,
              test_samples_1,
-             pred_samples,
-             test_samples_2]
+             test_samples_2,
+             test_samples_3]
 };
 timeline.push(preload);
 
@@ -159,15 +254,15 @@ for (let i = 0; i < test_samples_1.length; i++) {
   test_stimuli_1.push({stimulus: test_samples_1[i], correct_response: labels_1[i]});
 };
 
-var pred_stimuli = [];
-for (let i = 0; i < pred_samples.length; i++) {
-  pred_stimuli.push({stimulus: pred_samples[i], correct_response: classes[i],
-                     pred_class: preds[i], pred_concept: exps[i]});
-};
-
 var test_stimuli_2 = [];
 for (let i = 0; i < test_samples_2.length; i++) {
-  test_stimuli_2.push({stimulus: test_samples_2[i], correct_response: labels_2[i]});
+  test_stimuli_2.push({stimulus: test_samples_2[i], correct_response: labels_2[i],
+                       pred_class: preds_2[i], pred_concept: exps_2[i]});
+};
+
+var test_stimuli_3 = [];
+for (let i = 0; i < test_samples_3.length; i++) {
+  test_stimuli_3.push({stimulus: test_samples_3[i], correct_response: labels_3[i]});
 };
 
 // Define fixation
@@ -264,7 +359,7 @@ if(assigned_group === 'A') {
 
 var test_procedure_with_pred = {
     timeline: [fixation, test_with_pred],
-    timeline_variables: pred_stimuli,
+    timeline_variables: test_stimuli_2,
     repetitions: 1,
     randomize_order: true
 };
@@ -308,7 +403,7 @@ var test_no_model_2 = {
 
 var test_procedure_no_model_2 = {
     timeline: [fixation, test_no_model_2],
-    timeline_variables: test_stimuli_2,
+    timeline_variables: test_stimuli_3,
     repetitions: 1,
     randomize_order: true
 };
