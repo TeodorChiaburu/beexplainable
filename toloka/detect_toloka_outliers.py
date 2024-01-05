ab43996aec78106ae39d8ea0d5906022d8c637a1
Teo <teo_chiaburu@yahoo.com>
2024-01-05 16:34:15 +0100

    Automatic verdicts for tolokers

--- toloka/detect_toloka_outliers.py
+++ toloka/detect_toloka_outliers.py
@@ -4,8 +4,12 @@ import pandas as pd
 PATH_EXP = 'bees/Toloka_Bees_Control_100_all.csv'
 stats = pd.read_csv(PATH_EXP)
 
-outlier_ids = []
-for worker_id, group in stats.groupby(by='ASSIGNMENTworker_id'):
+review = open('bees/Toloka_Bees_Control_100_reviews.tsv', 'w')
+review.write('ASSIGNMENT:assignment_id\tACCEPT:verdict\tACCEPT:comment\n')
+
+# Note: since the overlapping in Toloka is 1, every worker_id will be matched with a unique assignment_id
+num_rejected = 0
+for assign_id, group in stats.groupby(by='ASSIGNMENTassignment_id'):
 
     # Look at every user and divide their answers into Tasks 1-2-3
     t1, t2, t3 = group.iloc[:10, 1:4], group.iloc[10:20, 1:4], group.iloc[20:30, 1:4]
@@ -13,12 +17,14 @@ for worker_id, group in stats.groupby(by='ASSIGNMENTworker_id'):
     num_corr_t2 = (t2['OUTPUTresult'] == t2['GOLDENresult']).sum()
     num_corr_t3 = (t3['OUTPUTresult'] == t3['GOLDENresult']).sum()
 
-    # A user will be removed, if they didn't get at least 4 correct answers per task
+    # A user will be rejected, if they didn't get at least 4 correct answers per task
+    # Rejected users get a '-' as verdict and a comment. Accepted users get a '+'
     if num_corr_t1 < 4 or num_corr_t2 < 4 or num_corr_t3 < 4:
-        outlier_ids.append(worker_id)
+        review.write(assign_id + '\t' + '-' + '\t' +
+                     'Sorry, you had too many wrong answers. We expect at least 4 correct answers out of 10 in each task.\n')
+        num_rejected += 1
+    else:
+        review.write(assign_id + '\t' + '+' + '\t' + '\n')
 
-print('Out of ' + str(stats['ASSIGNMENTworker_id'].nunique()) + ' users, ' + str(len(outlier_ids)) + ' will be removed.')
-
-with open('Toloka_Bees_Control_100_outliers', 'w') as f:
-    for id in outlier_ids:
-        f.write(id + '\n')
+print('Out of ' + str(stats['ASSIGNMENTassignment_id'].nunique()) + ' users, ' + str(num_rejected) + ' will be rejected.')
+review.close()
