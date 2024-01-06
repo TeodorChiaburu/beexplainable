db6464186c29dcb15235b6cdcfa165c0293f1a3a
Teo <teo_chiaburu@yahoo.com>
2024-01-06 16:19:10 +0100

    Stats Toloka Bees 100

--- toloka/detect_toloka_outliers.py
+++ toloka/detect_toloka_outliers.py
@@ -1,21 +1,22 @@
 """Script to identify unattentive/uninterested Tolokers that have submitted too many wrong answers"""
 import pandas as pd
 
-PATH_EXP = 'bees/Toloka_Bees_Control_100_all.csv'
-stats = pd.read_csv(PATH_EXP)
+exp_group = 'Examples' # Control, Concepts or Examples
+PATH_EXP = 'bees/Toloka_Bees_' + exp_group + '_100_all.tsv'
+stats = pd.read_csv(PATH_EXP, sep = '\t')
 
-review = open('bees/Toloka_Bees_Control_100_reviews.tsv', 'w')
+review = open('bees/Toloka_Bees_' + exp_group + '_100_reviews.tsv', 'w')
 review.write('ASSIGNMENT:assignment_id\tACCEPT:verdict\tACCEPT:comment\n')
 
 # Note: since the overlapping in Toloka is 1, every worker_id will be matched with a unique assignment_id
 num_rejected = 0
-for assign_id, group in stats.groupby(by='ASSIGNMENTassignment_id'):
+for assign_id, group in stats.groupby(by='ASSIGNMENT:assignment_id'):
 
     # Look at every user and divide their answers into Tasks 1-2-3
-    t1, t2, t3 = group.iloc[:10, 1:4], group.iloc[10:20, 1:4], group.iloc[20:30, 1:4]
-    num_corr_t1 = (t1['OUTPUTresult'] == t1['GOLDENresult']).sum()
-    num_corr_t2 = (t2['OUTPUTresult'] == t2['GOLDENresult']).sum()
-    num_corr_t3 = (t3['OUTPUTresult'] == t3['GOLDENresult']).sum()
+    t1, t2, t3 = group.iloc[:10,:], group.iloc[10:20,:], group.iloc[20:30,:]
+    num_corr_t1 = (t1['OUTPUT:result'] == t1['GOLDEN:result']).sum()
+    num_corr_t2 = (t2['OUTPUT:result'] == t2['GOLDEN:result']).sum()
+    num_corr_t3 = (t3['OUTPUT:result'] == t3['GOLDEN:result']).sum()
 
     # A user will be rejected, if they didn't get at least 4 correct answers per task
     # Rejected users get a '-' as verdict and a comment. Accepted users get a '+'
@@ -26,5 +27,5 @@ for assign_id, group in stats.groupby(by='ASSIGNMENTassignment_id'):
     else:
         review.write(assign_id + '\t' + '+' + '\t' + '\n')
 
-print('Out of ' + str(stats['ASSIGNMENTassignment_id'].nunique()) + ' users, ' + str(num_rejected) + ' will be rejected.')
+print('Out of ' + str(stats['ASSIGNMENT:assignment_id'].nunique()) + ' users, ' + str(num_rejected) + ' will be rejected.')
 review.close()
